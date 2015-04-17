c29bd4601a0b13490ebbd75d8820944d05ecd343
JUN FENG <alexfengau@gmail.com>
2015-04-17 15:26:54 +1000

    Enable session for adding items to the cart

--- cart.php
+++ cart.php
@@ -12,51 +12,67 @@
 		<div id="body">
 		<?php
 			$product_id = $_REQUEST['product_id'];
-			$quantity_to_purchase = $_REQUEST['quantity_to_purchase'];
-			echo "product_id is " . $product_id;
-			echo "<br>";
-			echo "quantity_to_purchase is " . $quantity_to_purchase;
+			$quantity_to_purchase = ltrim($_REQUEST['quantity_to_purchase'], '0');
+			// echo "product_id is " . $product_id;
+			// echo "<br>";
+			// echo "quantity_to_purchase is " . $quantity_to_purchase;
+
+			session_start();
+			if(!is_array($_SESSION['cart'])) {
+				$_SESSION['cart']=array();
+			}
+			array_push($_SESSION['cart'],array($product_id => $quantity_to_purchase));
+			// echo "SESSION['cart'] size is " . count($_SESSION['cart'])."\n";
+			// foreach ($_SESSION['cart'] as $item) {
+			// 	foreach ($item as $key => $value) {
+			// 	echo "product_id is $key\n";
+			// 	echo "quantity_to_purchase is $value\n";
+			// 	}
+			// }
 
 	        $link = mysql_connect("rerun","potiro","pcXZb(kL");
 	        if (!$link)
 	           die("Could not connect to Server");
 	        mysql_select_db("poti",$link);
+		?>	
 
-	        $query_string = "select * from products where product_id = $product_id";
-	        $result = mysql_query($query_string);
+		<form name="cart" action="cart.php">
+		<?php
+            print "<table>";
+            print "<tr>\n<th>Product Name</th><th>Unit Quantity</th><th>Unit Price</th><th>Quantity</th><th>Line Total</th></tr>";
 
-	        $num_rows = mysql_num_rows($result);
-	        if ($num_rows > 0 ) {
+		 	if(is_array($_SESSION['cart'])){
+		 		$totalPrice = 0;
+				foreach ($_SESSION['cart'] as $item) {
+					foreach ($item as $key => $value) {
+				        $query_string = "select * from products where product_id = $key";
+				        $result = mysql_query($query_string);
 
-	            print "<table>";
-	            print "<tr>\n<th>Product Name</th><th>Unit Quantity</th><th>Unit Price</th><th>Quantity</th><th>Line Total</th></tr>";
-	            while ( $a_row = mysql_fetch_assoc($result) ) {
-	                 print "<tr>\n";
-	                 print "<td>".$a_row['product_name']."</td>";
-	                 print "<td>".$a_row['unit_quantity']."</td>";
-	                 print "<td>".$a_row['unit_price']."</td>";
-	                 print "<td>".$quantity_to_purchase."</td>";
-	                 print "<td>".$a_row['unit_price']*$quantity_to_purchase."</td>";
-	                 print "</tr>";
-	            }
-	            print "<tr><td colspan='5' class='button'>Total Price for shopping: $</td></tr>";
-	            print "</table>";
+				        $num_rows = mysql_num_rows($result);
+				        if ($num_rows > 0 ) {
+				            while ( $a_row = mysql_fetch_assoc($result) ) {
+				                 print "<tr>\n";
+				                 print "<td>".$a_row['product_name']."</td>";
+				                 print "<td>".$a_row['unit_quantity']."</td>";
+				                 print "<td>".$a_row['unit_price']."</td>";
+				                 print "<td>".$value."</td>";
+				                 print "<td>".$a_row['unit_price']*$value."</td>";
+				                 print "</tr>";
+				                 $totalPrice += $a_row['unit_price']*$value;
+				            }			         			
+					 	}
+					}
+				}
 	        }
-	        mysql_close($link);
-	    ?>		
 
-			<form action="checkout.php">
-			<table>
-				<tr>
-					<td class='button'>
-						<input type="submit" name="checkout" value="Checkout">
-					</td>
-					<td class='button'>
-						 <input type="reset" name="Clear" value="Clear">
-					</td>
-				</tr>
-			</table>
-			</form>
+	        print "<tr><td colspan='5' class='button'>Total Price for shopping: $ ".$totalPrice."</td></tr>";
+	        print "<tr><td colspan='4' class='button'><input type='submit' value='Checkout'>";
+			print "<td class='button'><input type='button' value='Clear' onclick='document.cart.reset();return false;''></tr>";	       
+            print "</table>";
+
+        	mysql_close($link);
+	    ?>
+		</form>
 
 		</div>
 	</div>
